05f2bba49323d67f26aee950e34156310fe86ac8
nhtfw <huuthangfw@gmail.com>
2025-02-11 19:38:20 +0700

    tao du an BE

---
+++ BE/KLTND/src/main/java/com/vlearning/KLTND/KltndApplication.java
@@ -0,0 +1,13 @@
+package com.vlearning.KLTND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KltndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KltndApplication.class, args);
+	}
+
+}
---
+++ BE/KLTND/src/test/java/com/vlearning/KLTND/KltndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlearning.KLTND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KltndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
