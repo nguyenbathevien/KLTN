b646b530fcd7d432e2bd0917309c38a4a4d5791b
viễn <thevien2k3@gmail.com>
2025-03-06 17:03:58 +0700

    Resolve merge conflict in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import ScrollToTop from './components/common/ScrollToTop'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./auth.css";  // Thêm từ Incoming Change
+import "./swiper.css"; // Thêm từ Incoming Change
+import App from "./App.jsx";
+import ScrollToTop from "./components/common/ScrollToTop";
 import { unstable_HistoryRouter as HistoryRouter } from "react-router-dom";
-import { navigateHistory } from './utils/setting'
-import { Provider } from "react-redux"
-import { Store } from './redux/Store'
-createRoot(document.getElementById('root')).render(
+import { navigateHistory } from "./utils/setting";
+import { Provider } from "react-redux";
+import Store from "./redux/Store";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={Store}>
       <HistoryRouter history={navigateHistory}>
@@ -16,18 +18,5 @@ createRoot(document.getElementById('root')).render(
         <ScrollToTop />
       </HistoryRouter>
     </Provider>
-  </StrictMode>,
-)
-=======
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import "./auth.css";
-import "./swiper.css";
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
   </StrictMode>
 );
->>>>>>> 495de59f44ed06424c181e3f9efb8a5c2a3236e9
